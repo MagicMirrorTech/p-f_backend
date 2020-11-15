6644f61b2453f8d09eebd4e5b7e677f4e5ff89ec
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2020-11-15 15:09:28 -0600

    module time

--- controllers/times.js
+++ controllers/times.js
@@ -10,15 +10,12 @@ exports.getAllTimes = (req, res, next) => {
 exports.createTimes = (req, res, next) => {
     const date = new Date()
     User.findOne({"pin": req.body.pin}).then(user =>{
-        console.log("user >>>>>>> ", user)
         Times.findOne({"workerId": user}).exists('timeOut', false).then( async time =>{
-            console.log("time>>>>>>>>>> ", time)
             time.timeOut = date
             time.dateOut = date
             await time.save()
             res.status(200).json({ time })
         }).catch(exception=>{
-            console.log("exception>>>>>>>>> ", exception)
             let time = new Times({workerId: user._id, date: date, timeIn: date})
             Times.create(time)
                 .then(time => {
